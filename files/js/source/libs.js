33378a0a5d3b63ea3444b411afdeb3073a3d089f
Edgar Pino <edgarbeltran@me.com>
2015-04-03 23:50:22 -0500

    Reverted added files by mistake

--- files/js/source/libs.js
+++ files/js/source/libs.js
@@ -2,4 +2,3 @@
 //@prepros-append libraries/_selectivizr.js
 //@prepros-append libraries/_jquery.justifiedGallery.min.js
 //@prepros-append libraries/_jquery.swipebox.js
-//@prepros-append libraries/_cropper.js
